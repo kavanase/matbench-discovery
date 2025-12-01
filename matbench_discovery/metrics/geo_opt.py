d6c5255f58dbd5398f0992ba0285d9c085aebb4a
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2025-12-01 12:19:05 -0500

    SRME and geo opt evals, `Key.mat_id` update, ensure floats

--- matbench_discovery/metrics/geo_opt.py
+++ matbench_discovery/metrics/geo_opt.py
@@ -32,11 +32,11 @@ def write_metrics_to_yaml(
 
     # Get metrics for this model
     metrics_for_symprec = {
-        str(Key.rmsd): round(df_geo_opt[MbdKey.structure_rmsd_vs_dft].iloc[0], 4),
-        str(Key.n_sym_ops_mae): round(df_geo_opt[Key.n_sym_ops_mae].iloc[0], 4),
-        str(Key.symmetry_decrease): round(df_geo_opt[Key.symmetry_decrease].iloc[0], 4),
-        str(Key.symmetry_match): round(df_geo_opt[Key.symmetry_match].iloc[0], 4),
-        str(Key.symmetry_increase): round(df_geo_opt[Key.symmetry_increase].iloc[0], 4),
+        str(Key.rmsd): float(round(df_geo_opt[MbdKey.structure_rmsd_vs_dft].iloc[0], 4)),
+        str(Key.n_sym_ops_mae): float(round(df_geo_opt[Key.n_sym_ops_mae].iloc[0], 4)),
+        str(Key.symmetry_decrease): float(round(df_geo_opt[Key.symmetry_decrease].iloc[0], 4)),
+        str(Key.symmetry_match): float(round(df_geo_opt[Key.symmetry_match].iloc[0], 4)),
+        str(Key.symmetry_increase): float(round(df_geo_opt[Key.symmetry_increase].iloc[0], 4)),
         str(Key.n_structures): int(df_geo_opt[Key.n_structures].iloc[0]),
         "analysis_file": analysis_file_path,
         "analysis_file_url": None,  # to be filled after uploading to figshare
@@ -58,6 +58,7 @@ def write_metrics_to_yaml(
         if unit := metric_units.get(key):
             metrics_for_symprec.yaml_add_eol_comment(unit, key, column=1)
 
+    print(metrics_for_symprec)
     update_yaml_file(
         model.yaml_path, f"metrics.geo_opt.{symprec_key}", metrics_for_symprec
     )
